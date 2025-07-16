3dd96a1a08875ac599992e895614e4e3a3d3ef74
fiignacio <62571351+fiignacio@users.noreply.github.com>
2025-07-16 13:14:33 -0600

    new build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Asegúrate de tener un archivo index.css para los estilos de Tailwind
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
