6dac4b3189945ff258db2a253245b47b4c068b0d
fiignacio <62571351+fiignacio@users.noreply.github.com>
2025-07-15 19:52:11 -0600

    Update Spinner.jsx

--- components/Spinner.jsx
+++ components/Spinner.jsx
@@ -1,1 +1,6 @@
-
+// Spinner.jsx
+const Spinner = () => (
+    <div className="flex justify-center items-center p-4">
+        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-500"></div>
+    </div>
+);
