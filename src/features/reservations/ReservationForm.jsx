2a2da35e13fe0311f29c88db8ed77345f5ab81c1
fiignacio <62571351+fiignacio@users.noreply.github.com>
2025-07-16 18:21:11 -0600

    Update ReservationForm.jsx

--- src/features/reservations/ReservationForm.jsx
+++ src/features/reservations/ReservationForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useReservationsContext } from '../../context/ReservationsContext';
 import Button from '../../components/ui/Button';
 import Input from '../../components/ui/Input';
-import { format, parseISO, isAfter, differenceInNights } from 'date-fns';
+import { format, parseISO, isAfter, differenceInWeeks  } from 'date-fns';
 
 
 const ReservationForm = ({ reservation, onDone }) => {
